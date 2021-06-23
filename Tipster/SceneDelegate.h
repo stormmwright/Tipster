81858cd33f6c938d1e654e1e711d0bb1452f2642
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-06-22 20:10:30 -0400

    init commit

---
+++ Tipster/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Tipster
+//
+//  Created by Storm Wright on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Tipster/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Tipster
+//
+//  Created by Storm Wright on 6/22/21.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

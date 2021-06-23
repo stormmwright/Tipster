d89da60409f099be8f66c8fb211b8d6b7713f09b
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-06-22 22:35:38 -0400

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Tipster
+# Project 1 - Tipster
+
+Tipster is a tip calculator application for iOS.
+
+Submitted by: Storm Wright
+
+Time spent: 4 hours spent in total
+
+## User Stories
+
+The following **required** functionality is complete:
+
+* [ ] User can enter a bill amount, choose a tip percentage, and see the tip and total values.
+
+The following **optional** features are implemented:
+
+N/A
+
+The following **additional** features are implemented:
+
+N/A
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [Kap](https://getkap.co/).
+
+## Notes
+
+Normally I excel in UI design, but because I spent a couple of hours figuring out an issue with synching my local repository with my remote repository, I didn't have as much time to implement the optional and additional features. On the next assignment, I will practice better time management.
+
+## Credits
+
+N/A
+
+- [AFNetworking](https://github.com/AFNetworking/AFNetworking) - networking task library
+
+## License
+
+    Copyright [N/A] [N/A]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- Tipster/TipViewController.m
+++ Tipster/TipViewController.m
@@ -8,6 +8,11 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPecentageControl;
+@property (weak, nonatomic) IBOutlet UIView *labelsContainerView;
 
 @end
 
@@ -17,6 +22,61 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:true];
+}
+
+
+- (IBAction)updateLabels:(id)sender {
+//    if(self.billField.text.length == 0) {
+//        [self hideLabels];
+//    } else if(self.billField.text.length > 0) {
+//        [self showLabels];
+//    }
+        
+    double tipPercentages[] = {0.15, 0.2, 0.25};
+    double tipPercentage = tipPercentages[self.tipPecentageControl.selectedSegmentIndex];
+    
+    double bill = [self.billField.text doubleValue];
+    double tip = bill * tipPercentage;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
+
+//- (void)hideLabels {
+//    [UIView animateWithDuration:0.5 animations:^{
+//        CGRect billFrame = self.billField.frame;
+//        billFrame.origin.y += 200;
+//
+//        self.billField.frame = billFrame;
+//
+//        CGRect labelsFrame = self.labelsContainerView.frame;
+//        labelsFrame.origin.y += 200;
+//
+//        self.labelsContainerView.frame = labelsFrame;
+//
+//        self.labelsContainerView.alpha = 0;
+//    }];
+//}
+
+//- (void)showLabels {
+//    [UIView animateWithDuration:0.5 animations:^{
+//        CGRect billFrame = self.billField.frame;
+//        billFrame.origin.y += 200;
+//
+//        self.billField.frame = billFrame;
+//
+//        CGRect labelsFrame = self.labelsContainerView.frame;
+//        labelsFrame.origin.y += 200;
+//
+//        self.labelsContainerView.frame = labelsFrame;
+//
+//        self.labelsContainerView.alpha = 0;
+//    }];
+//}
 
 /*
 #pragma mark - Navigation

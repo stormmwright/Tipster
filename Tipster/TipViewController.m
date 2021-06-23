83a4895daead41b652186cc4bec27720f8e991f1
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-06-22 22:27:57 -0400

    implemented basic tip calculator

---
+++ Tipster/TipViewController.m
@@ -0,0 +1,31 @@
+//
+//  TipViewController.m
+//  Tipster
+//
+//  Created by Storm Wright on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
